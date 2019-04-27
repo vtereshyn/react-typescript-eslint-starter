13dca8adbc3b7b76ede3b8e0d8c5492733dca7d9
Vladyslav Tereshyn <vladyslav.tereshyn@gmail.com>
2019-04-27 17:00:22 +0300

    feat: deploy with header

---
+++ node_modules/microevent.ts/lib/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var Event_1 = require('./Event');
+exports.Event = Event_1.default;
---
+++ node_modules/worker-rpc/lib/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var RpcProvider_1 = require('./RpcProvider');
+exports.RpcProvider = RpcProvider_1.default;
